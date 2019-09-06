a7c352765ee64a65e3998b725f107605fa0a0b29
anthony <nvhoanganh1909@gmail.com>
2019-09-06 17:44:25 +1000

    working one

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-const data = 'hello, theLre,how, a';
-var R = require('rambda');
+var R = require('ramda');
 
 const splitTrimUpper = R.pipe(
   R.split(','),
@@ -10,6 +9,61 @@ const splitTrimUpper = R.pipe(
     )
   )
 );
+console.log(splitTrimUpper('a,b , c'))
 
-console.log(splitTrimUpper(data).indexOf('A'));
-console.log(R.indexOf('A', splitTrimUpper(data)));
+const MAX_COUNT = 3;
+const spacer = R.join(' ');
+
+console.log(spacer(['a', 'b']))
+
+const getPersonName = R.ifElse(
+  R.has('lastName'),
+  R.compose(
+    R.join(' '),
+    R.props(['firstName', 'lastName'])
+  ),
+  R.prop('firstName')
+);
+
+
+console.log(
+  getPersonName({
+    firstName: 'anthony',
+    lastName: 'nguyen'
+  })
+);
+
+
+const isLongerThanLimit = data =>
+data.length > 1 && data.length <= MAX_COUNT + 1;
+
+console.log(isLongerThanLimit([1,3,4]))
+
+console.log(isLongerThanLimit([1,3,4,5,6]))
+
+
+const getNamesForShortList = R.pipe(() => console.log('short'));
+
+const getNamesForLongList = R.pipe(() => console.log('long'));
+
+const getNames = R.pipe(
+  R.ifElse(isLongerThanLimit, getNamesForShortList, getNamesForLongList)
+);
+
+console.log(getNames([
+  {
+    firstName: 'Anthony'
+  },
+  {
+    firstName: 'Brent'
+  },
+  {
+    firstName: 'David'
+  },
+  {
+    firstName: 'William'
+  },
+  {
+    firstName: 'Bupa'
+  }
+]))
